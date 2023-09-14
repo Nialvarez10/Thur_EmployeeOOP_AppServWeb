bfd751d34c4f0b63178bea09f5eabb4a9a821096
Estefania <nialvarez10@gmail.com>
2023-09-14 18:21:31 -0500

    Exceptions

--- EmployeeOOP/Program.cs
+++ EmployeeOOP/Program.cs
@@ -1,9 +1,29 @@
 ﻿using EmployeeOOP.Classes;
+try
+{
+    Console.WriteLine("OPP APPLICATION");
+    Console.WriteLine("---------------");
+    int day, month, year;
 
-Console.WriteLine("OPP APPLICATION");
-Console.WriteLine("---------------");
+    Console.Write("Ingresar el dia: ");
+    day = Convert.ToInt32(Console.ReadLine());
 
-//Inicialización clase Date
-Date dateObject = new Date(4,5,56665214);
-Console.WriteLine(dateObject.ToString());
+    Console.Write("Ingresar el mes: ");
+    month = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el año: ");
+    year = Convert.ToInt32(Console.ReadLine());
+
+    //Inicialización clase Date
+    Date dateObject = new Date(day, month, year);
+    Console.WriteLine("/n");
+    Console.WriteLine(dateObject.ToString());
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+
+}
+
 
